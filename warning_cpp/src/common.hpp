12195789444c639ebcf21c8fe7cc7b6d130e81da
Michal Sojka <michal.sojka@cvut.cz>
2022-08-16 12:52:30 +0200

    Report IP address parsing failures

--- warning_cpp/src/common.hpp
+++ warning_cpp/src/common.hpp
@@ -8,7 +8,7 @@
 constexpr char IPA2X_GUID_PREFIX[] = "69.70.61.32.78.5F.63.76.75.74.be.ef";
 
 void addCommonDdsArguments(argparse::ArgumentParser& program);
-std::vector<int> parseIP(std::string address);
+std::vector<int> parseIP(const std::string &address, uint16_t default_port = 11811);
 
 
 #endif
