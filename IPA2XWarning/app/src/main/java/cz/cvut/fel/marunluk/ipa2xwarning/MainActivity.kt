6ef1901486e0ca9a1f4214684dfd8ffd05faac1b
Lukáš Maruniak <marunluk@fel.cvut.cz>
2022-08-18 12:16:11 +0200

    Modify android application to remeber status from previous instance
    Application now remembers if discovery server was used or not and which IP address was entered. Default value is no server and IP 192.168.162.10:11811

--- IPA2XWarning/app/src/main/java/cz/cvut/fel/marunluk/ipa2xwarning/MainActivity.kt
+++ IPA2XWarning/app/src/main/java/cz/cvut/fel/marunluk/ipa2xwarning/MainActivity.kt
@@ -1,6 +1,7 @@
 package cz.cvut.fel.marunluk.ipa2xwarning
 
 import android.Manifest
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.content.res.Configuration
 import android.graphics.drawable.Animatable2
@@ -13,6 +14,7 @@ import android.media.MediaPlayer
 import android.os.Bundle
 import android.text.InputFilter
 import android.text.Spanned
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -30,6 +32,7 @@ class MainActivity : AppCompatActivity(), LocationListener {
     private lateinit var locationManager: LocationManager
     private lateinit var mp1: MediaPlayer
     private lateinit var mp2: MediaPlayer
+    private lateinit var prefs: SharedPreferences
 
     private var currentState: Int = 0
 
@@ -40,7 +43,7 @@ class MainActivity : AppCompatActivity(), LocationListener {
     var clongitude: AtomicLong = AtomicLong(0)
     var clatitude: AtomicLong = AtomicLong(0)
     var speed: AtomicInteger = AtomicInteger(0)
-    var IPaddress: IntArray = intArrayOf(192, 168, 162, 10, 11811)
+    var IPaddress: IntArray = intArrayOf(0, 0, 0, 0, 0)
     var started: Boolean = false
 
     // Threads runnables
@@ -55,6 +58,13 @@ class MainActivity : AppCompatActivity(), LocationListener {
 
         super.onCreate(savedInstanceState)
 
+        prefs = getSharedPreferences("ipa2x_prefs", MODE_PRIVATE)
+        IPaddress[0] = prefs.getInt("ip0", 192)
+        IPaddress[1] = prefs.getInt("ip1", 168)
+        IPaddress[2] = prefs.getInt("ip2", 162)
+        IPaddress[3] = prefs.getInt("ip3", 10)
+        IPaddress[4] = prefs.getInt("ip4", 11811)
+
         configure()
 
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
@@ -66,9 +76,6 @@ class MainActivity : AppCompatActivity(), LocationListener {
         mp1 = MediaPlayer.create(this, R.raw.chime1a)
         mp2 = MediaPlayer.create(this, R.raw.chime2a)
 
-        // Start handler threads
-        //pubHandlerT.start()
-        //subHandlerT.start()
 
     }
 
@@ -79,6 +86,7 @@ class MainActivity : AppCompatActivity(), LocationListener {
         drawDangerImage(currentState)
     }
 
+    // Stop and terminte app
     override fun onDestroy() {
         super.onDestroy()
         // Send terminate singal to threads
@@ -112,6 +120,7 @@ class MainActivity : AppCompatActivity(), LocationListener {
 
     }
 
+    // Init all GUI fields when constructing activity
     private fun configure() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -125,22 +134,15 @@ class MainActivity : AppCompatActivity(), LocationListener {
         binding.crossingLatitudeText.text = "Crossing Latitude: ${longBitsToDouble(clatitude.get())}"
         binding.crossingLongitudeText.text = "Crossing Longitude: ${longBitsToDouble(clongitude.get())}"
 
+        binding.modeSwitch.isChecked = prefs.getBoolean("switch", false)
         binding.ipA.filters = arrayOf<InputFilter>(IPFilter())
         binding.ipB.filters = arrayOf<InputFilter>(IPFilter())
         binding.ipC.filters = arrayOf<InputFilter>(IPFilter())
         binding.ipD.filters = arrayOf<InputFilter>(IPFilter())
-        binding.ipA.isEnabled = false
-        binding.ipB.isEnabled = false
-        binding.ipC.isEnabled = false
-        binding.ipD.isEnabled = false
-        binding.port.isEnabled = false
+        binding.port.filters = arrayOf<InputFilter>(PortFilter())
+        manageIPFields(prefs.getBoolean("switch", false))
 
         if (started) {
-            /*binding.ipA.isEnabled = false
-            binding.ipB.isEnabled = false
-            binding.ipC.isEnabled = false
-            binding.ipD.isEnabled = false
-            binding.port.isEnabled = false*/
             binding.startButton.isEnabled = false
             binding.modeSwitch.isEnabled = false
         }
@@ -149,79 +151,90 @@ class MainActivity : AppCompatActivity(), LocationListener {
             finishAndRemoveTask()
         }
         
-        binding.modeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->  //Line A
-            if (isChecked) {
-                binding.ipA.isEnabled = true
-                binding.ipB.isEnabled = true
-                binding.ipC.isEnabled = true
-                binding.ipD.isEnabled = true
-                binding.port.isEnabled = true
-                binding.ipA.setText(IPaddress[0].toString())
-                binding.ipB.setText(IPaddress[1].toString())
-                binding.ipC.setText(IPaddress[2].toString())
-                binding.ipD.setText(IPaddress[3].toString())
-                binding.port.setText(IPaddress[4].toString())
-            } else {
-                binding.ipA.isEnabled = false
-                binding.ipB.isEnabled = false
-                binding.ipC.isEnabled = false
-                binding.ipD.isEnabled = false
-                binding.port.isEnabled = false
-                binding.ipA.text.clear()
-                binding.ipB.text.clear()
-                binding.ipC.text.clear()
-                binding.ipD.text.clear()
-                binding.port.text.clear()
-            }
+        binding.modeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            prefs.edit().putBoolean("switch", isChecked).commit()
+            manageIPFields(isChecked)
         }
 
         binding.startButton.setOnClickListener {
-
-            started = true
-            readIP()
-            binding.ipA.isEnabled = false
-            binding.ipB.isEnabled = false
-            binding.ipC.isEnabled = false
-            binding.ipD.isEnabled = false
-            binding.port.isEnabled = false
-            binding.startButton.isEnabled = false
-            binding.modeSwitch.isEnabled = false
-
-            pubHandler.setIP(binding.modeSwitch.isChecked, IPaddress)
-            subHandler.setIP(binding.modeSwitch.isChecked, IPaddress)
-
-            // Start handler threads
-            pubHandlerT.start()
-            subHandlerT.start()
+            verifyAndStartCommunication()
         }
+    }
 
+    // Checks if IP is valid and starts DDS handler threads
+    fun verifyAndStartCommunication() {
+        if (binding.modeSwitch.isChecked) {
+            if (readIP()) {
+                prefs.edit().putInt("ip0", IPaddress[0]).commit()
+                prefs.edit().putInt("ip1", IPaddress[1]).commit()
+                prefs.edit().putInt("ip2", IPaddress[2]).commit()
+                prefs.edit().putInt("ip3", IPaddress[3]).commit()
+                prefs.edit().putInt("ip4", IPaddress[4]).commit()
+            } else {
+                return
+            }
+        }
+        started = true
+
+        binding.ipA.isEnabled = false
+        binding.ipB.isEnabled = false
+        binding.ipC.isEnabled = false
+        binding.ipD.isEnabled = false
+        binding.port.isEnabled = false
+        binding.startButton.isEnabled = false
+        binding.modeSwitch.isEnabled = false
+
+        pubHandler.setIP(binding.modeSwitch.isChecked, IPaddress)
+        subHandler.setIP(binding.modeSwitch.isChecked, IPaddress)
+
+        // Start handler threads
+        pubHandlerT.start()
+        subHandlerT.start()
+    }
+
+    // Disable or enable (and put values from memory) IP fields
+    fun manageIPFields(status: Boolean) {
+
+        binding.ipA.isEnabled = status
+        binding.ipB.isEnabled = status
+        binding.ipC.isEnabled = status
+        binding.ipD.isEnabled = status
+        binding.port.isEnabled = status
+
+        if (status) {
+            binding.ipA.setText(IPaddress[0].toString())
+            binding.ipB.setText(IPaddress[1].toString())
+            binding.ipC.setText(IPaddress[2].toString())
+            binding.ipD.setText(IPaddress[3].toString())
+            binding.port.setText(IPaddress[4].toString())
+        } else {
+            binding.ipA.text.clear()
+            binding.ipB.text.clear()
+            binding.ipC.text.clear()
+            binding.ipD.text.clear()
+            binding.port.text.clear()
+        }
     }
 
     // Read and parse IP addresses
-    fun readIP() {
+    fun readIP(): Boolean {
         val ip_A = binding.ipA.text.toString().toIntOrNull()
         val ip_B = binding.ipB.text.toString().toIntOrNull()
         val ip_C = binding.ipC.text.toString().toIntOrNull()
         val ip_D = binding.ipD.text.toString().toIntOrNull()
         val port = binding.port.text.toString().toIntOrNull()
 
-        if ((port != null) && (port in 1024..65535)) {
-            IPaddress[4] = port
-        } else {
-            binding.port.setText(IPaddress[4].toString())
+        if ((ip_A == null) || (ip_B == null) || (ip_C == null) || (ip_D == null) || (port == null)) {
+            Toast.makeText(this@MainActivity, "Invalid IP or port!", Toast.LENGTH_SHORT).show()
+            return false
         }
+        IPaddress[0] = ip_A
+        IPaddress[1] = ip_B
+        IPaddress[2] = ip_C
+        IPaddress[3] = ip_D
+        IPaddress[4] = port
 
-        if ((ip_A == null) || (ip_B == null) || (ip_C == null) || (ip_D == null)) {
-            binding.ipA.setText(IPaddress[0].toString())
-            binding.ipB.setText(IPaddress[1].toString())
-            binding.ipC.setText(IPaddress[2].toString())
-            binding.ipD.setText(IPaddress[3].toString())
-        } else {
-            IPaddress[0] = ip_A
-            IPaddress[1] = ip_B
-            IPaddress[2] = ip_C
-            IPaddress[3] = ip_D
-        }
+        return true
     }
 
     // View and play warning based on boolean states
@@ -291,6 +304,7 @@ class MainActivity : AppCompatActivity(), LocationListener {
         }
     }
 
+    // Filter to allow only valid IP values
     inner class IPFilter() : InputFilter {
         private var ipMin: Int = 0
         private var ipMax: Int = 255
@@ -299,6 +313,23 @@ class MainActivity : AppCompatActivity(), LocationListener {
             try {
                 val input = Integer.parseInt(dest.toString() + source.toString())
                 if ((ipMin <= input) && (input <= ipMax)) {
+                    return null
+                }
+            } catch (e: NumberFormatException) {
+                e.printStackTrace()
+            }
+            return ""
+        }
+    }
+
+    // Filter to allow only valid port values
+    inner class PortFilter() : InputFilter {
+        private var portMax: Int = 65535
+
+        override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dStart: Int, dEnd: Int): CharSequence? {
+            try {
+                val input = Integer.parseInt(dest.toString() + source.toString())
+                if ((input <= portMax)) {
                     return null
                 }
             } catch (e: NumberFormatException) {
